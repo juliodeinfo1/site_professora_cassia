786d8d3cda63f0cea54c5798b6088b34cae76e14
juliodeinfo1 <juliocesardasilvapais3@gmail.com>
2025-10-17 17:01:42 +0000

    a

--- Doom/src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line
-/// <reference types="vite/client" />
-
-// eslint-disable-next-line no-unused-vars
-declare const JSDOS_VERSION: string;
--- Doom/src/window/error-window.tsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { Error } from "../components/error";
-import { State } from "../store";
-
-export function ErrorWindow() {
-    const error = useSelector((state: State) => state.dos.error);
-    return <Error error={error} />;
-}
-
